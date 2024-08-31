a352b255197d8cb1e11efa8a00662fe9354fbcc0
Ulug'bek <119843506+ulugbekhusainovv@users.noreply.github.com>
2024-08-31 13:43:28 +0500

    feat(reverse and rotate)

--- 42/main.py
+++ 42/main.py
@@ -13,24 +13,6 @@ Misol 1:
 Kiritish: nums = [0]
 Natija: [0]"""
 
-
-"This is my solution algorithm"
-def moveZeroes(nums):
-    j = 0
-
-    for i in range(len(nums)):
-        if nums[i] != 0:
-            nums[j] = nums[i]
-            j += 1
-
-    for i in range(j, len(nums)):
-        nums[i] = 0
-
-    return nums
-
-
-
-"This is my teacher's solution"
 def moveZeroes(nums):
     count = 0
     for i, num in enumerate(nums):
@@ -39,5 +21,41 @@ def moveZeroes(nums):
             continue
         nums[i], nums[i-count] = nums[i-count], nums[i]
         
+    return nums
+
+
+"""Ro'yxatni aylantiring
+Berilgan ro'yxatni k qadam o'ng tomonga aylantiring.
+
+Ro'yxatni 1 qadam o'ng tomonga aylantirish, o'ng tomondagi ohirgi elementni, chap tomonning boshiga olib qo'yish degani.
+
+Amalni berilgan ro'yhat ustida bajaring - yangi ro'yxat yaratmang.
+
+Misol 1:
+Kiritish: nums = [1,2,3,4,5,6,7], k = 3
+Natija: [5,6,7,1,2,3,4]
+Tushuntirish:
+1-qadam: [7,1,2,3,4,5,6]
+2-qadam: [6,7,1,2,3,4,5]
+3-qadam: [5,6,7,1,2,3,4]
+Misol 2:
+Kiritish: nums = [-1,-100,3,99], k = 2
+Natija: [3,99,-1,-100]
+Tushuntirish: 
+1-qadam: [99,-1,-100,3]
+2-qadam: [3,99,-1,-100]"""
+
+def reverse(nums, i , j):
+    while i < j:
+        nums[i], nums[j] = nums[j], nums[i]
+        i += 1
+        j -= 1
+
+
+def rotate(nums: list, k: int):
+    k = k % len(nums)
+    reverse(nums, 0, len(nums) - 1)
+    reverse(nums, 0, k - 1)
+    reverse(nums, k, len(nums) - 1)
     return nums
 
